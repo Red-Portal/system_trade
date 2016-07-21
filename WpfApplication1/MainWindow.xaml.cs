f5dc2c655b6d82102046948abdb1aee010eeab5c
Red-Portal <msca8h@naver.com>
2016-07-21 16:28:13 +0900

    first commit, added KHOpenAPI ActiveX control to a WPF template

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Create the interop host control.
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+                new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            // Create the ActiveX control.
+            AxKHOpenAPILib.AxKHOpenAPI axKHOA = new AxKHOpenAPILib.AxKHOpenAPI();
+
+            // Assign the ActiveX control as the host control's child.
+            host.Child = axKHOA;
+
+            // Add the interop host control to the Grid
+            // control's collection of child controls.
+            this.grid1.Children.Add(host);
+
+            //////////////////////////////LOGIN COMMUNICATION///////////////////////////////
+            //creating OpenAPI instance
+            long Result;
+
+            Result = axKHOA.CommConnect();
+
+            if (Result != 0)
+                MessageBox.Show("Login창 열림 Fail");
+            else
+                MessageBox.Show("로그인 성공!");
+        }
+    }
+}
