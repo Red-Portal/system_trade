200004bb3944f66e61e465bbcc638e6e9a0c88e1
Red-Portal <msca8h@naver.com>
2016-07-22 02:02:17 +0900

    applied a different version of console style code

--- WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,12 @@ namespace WpfApplication1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ConsoleContent dc = new ConsoleContent();
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = dc;
+            Loaded += Window_Loaded;
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -41,14 +46,75 @@ namespace WpfApplication1
             // control's collection of child controls.
             this.grid1.Children.Add(host);
 
+            InputBlock.KeyDown += InputBlock_KeyDown;
+            InputBlock.Focus();
+
             //////////////////////////////LOGIN COMMUNICATION///////////////////////////////
             checkLoginComm(axKHOA.CommConnect());
 
+        }
+        void InputBlock_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key==Key.Enter)
+            {
+                dc.ConsoleInput = InputBlock.Text;
+                dc.RunCommand();
+                InputBlock.Focus();
+                Scroller.ScrollToBottom();
+            }
         }
         private void checkLoginComm(long Result)
         {
             if (Result != 0)
                 MessageBox.Show("Login창 열림 Fail");
+        }
+    }
+
+    public class ConsoleContent:INotifyPropertyChanged
+    {
+        string consoleInput = string.Empty;
+        ObservableCollection<string> consoleOutput = new ObservableCollection<string>()
+            {"Console Emulation Sample" };
+
+        public string ConsoleInput
+        {
+            get
+            {
+                return consoleInput;
+            }
+            set
+            {
+                consoleInput = value;
+                OnPropertyChanged("ConsoleInput");
+            }
+        }
+
+        public ObservableCollection<string> ConsoleOutput
+        {
+            get
+            {
+                return consoleOutput;
+            }
+            set
+            {
+                consoleOutput = value;
+                OnPropertyChanged("ConsoleOutput");
+            }
+        }
+
+        public void RunCommand()
+        {
+            ConsoleOutput.Add(ConsoleInput);
+            // do your stuff here.
+            ConsoleInput = String.Empty;
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged(string propertyName)
+        {
+            if (null != PropertyChanged)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
